235826ac5a6feb7a8ac2b10163148904c8ab5e7b
Andrii Kurkov <kurkovandrey90@gmail.com>
2024-11-21 19:59:08 +0200

    Add functionality for writing and reading files

---
+++ src/app/FileHandler.java
@@ -0,0 +1,37 @@
+package app;
+
+import java.io.*;
+
+public class FileHandler {
+
+    private final static String BASE_PATH = "files/";
+
+    public String writeFile(String fileName, String fileContent) {
+        try {
+            FileWriter fw = new FileWriter(BASE_PATH + fileName+ ".txt");
+            fw.write(fileContent);
+            fw.close();
+            return "Success.";
+        } catch (IOException e) {
+            return "Exception: " + e.getMessage();
+        }
+    }
+
+    public String readFile(String path) {
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(path);
+        try (FileReader reader = new FileReader(file)) {
+            int sym;
+            while ((sym = reader.read()) != -1) {
+                stringBuilder.append((char) sym);
+            }
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+        catch (IOException e) {
+            return e.getMessage();
+        }
+        return stringBuilder.toString();
+    }
+}
