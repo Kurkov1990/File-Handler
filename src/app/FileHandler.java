31bf23912df5dd04140d059a1a4aa1163501eb19
Andrii Kurkov <kurkovandrey90@gmail.com>
2024-11-22 18:57:20 +0200

    Refactor writeFile method

--- src/app/FileHandler.java
+++ src/app/FileHandler.java
@@ -7,10 +7,8 @@ public class FileHandler {
     private final static String BASE_PATH = "files/";
 
     public String writeFile(String fileName, String fileContent) {
-        try {
-            FileWriter fw = new FileWriter(BASE_PATH + fileName+ ".txt");
+        try (FileWriter fw = new FileWriter(BASE_PATH + fileName+ ".txt")){
             fw.write(fileContent);
-            fw.close();
             return "Success.";
         } catch (IOException e) {
             return "Exception: " + e.getMessage();
