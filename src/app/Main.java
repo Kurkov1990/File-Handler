f49db287a14f88ea8c1858978c6c794227d9610c
Andrii Kurkov <kurkovandrey90@gmail.com>
2024-11-25 19:26:11 +0200

    Add functionality for user typing for name and file content

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,18 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
     private final static String BASE_PATH = "files/";
 
     public static void main(String[] args) {
         FileHandler handler = new FileHandler();
-        String fileName = "myfile";
-        String fileContent = "My very important information.";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter file name: ");
+        String fileName = scanner.nextLine();
+        System.out.print("Enter text for adding to the file: ");
+        String fileContent = scanner.nextLine();
         String result = handler.writeFile(fileName, fileContent);
         String content = handler.readFile(BASE_PATH + fileName + ".txt");
         getOutput("RESULT: " + result);
